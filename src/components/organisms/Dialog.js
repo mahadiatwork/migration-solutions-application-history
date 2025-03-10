57e2bf947c31375eb1074c982fe8f1cf141f289d
Maddie H.(Work) <99705111+mahadiatwork@users.noreply.github.com>
2025-03-11 01:35:58 +0600

    updated the regarding logic

--- src/components/organisms/Dialog.js
+++ src/components/organisms/Dialog.js
@@ -1059,6 +1059,7 @@ export function Dialog({
               <RegardingField
                 formData={formData}
                 handleInputChange={handleInputChange}
+                selectedRowData={selectedRowData}
               />
             </Grid>
           </Grid>
